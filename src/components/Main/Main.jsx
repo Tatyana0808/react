dc8379114c2657e154957241cf56ede24a230fb2
Tatyana0808 <144019586+Tatyana0808@users.noreply.github.com>
2023-12-30 18:38:51 +0300

    сделала 2 пункт

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,22 @@
 import Column from "../Column/Column";
-
+const statusList = [
+  "Без статуса",
+  "Нужно сделать",
+  "В работе",
+  "Тестирование",
+  "Готово",
+  
+];
 function Main(){
     return (<main className="main">
     <div className="container">
       <div className="main__block">
         <div className="main__content">
-          <Column title={"Без статуса"}/>
-          <Column title={"Нужно сделать"}/>
-          <Column title={"В работе"}/>
-          <Column title={"Тестирование"}/>
-          <Column title={"Готово"}/>
+          {statusList.map((item) => (<Column 
+          key={item}
+          title={item}/>)
+          )}
+        
           
         </div>
       </div>
