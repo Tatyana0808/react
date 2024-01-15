3a634e9ef203bb9e005e020a52aedb8611726ae4
Tatyana0808 <144019586+Tatyana0808@users.noreply.github.com>
2024-01-15 15:27:18 +0300

    сделала компонент Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { Container } from "../Common/Common.styled";
+import { HeaderWrapper } from "./Header.styled";
 function Header({addCard}){
   const [isOpened,setIsOpened] = useState(false);
   function togglePopUp() {
     setIsOpened((prev) => !prev)
   }
     return (
-        <header className="header">
+        
+        <HeaderWrapper> 
     
     <Container>
       <div className="header__block">
@@ -46,7 +48,7 @@ function Header({addCard}){
         </nav>
       </div>
     </Container>
-  </header>
+    </HeaderWrapper> 
     )
 }
 export default Header;
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+
+export const HeaderWrapper = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  background-color: #FFFFFF;
+`;
