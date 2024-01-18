86fae85a75790de34a08947c7b0fde91fcc451f9
Tatyana0808 <144019586+Tatyana0808@users.noreply.github.com>
2024-01-18 12:42:01 +0300

    сделала приватный маршрут

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,57 @@
 import { useEffect, useState } from 'react';
 import { cardList } from './data';
 import { GlobalStyle } from './Global.styled';
-import MainPage from './pages/MainPage';
+import { MainPage } from './pages/MainPage';
 import { Route, Routes } from 'react-router-dom';
 import { appRoutes } from './lib/appRoutes';
-import CardPage from './pages/CardPage';
+import { CardPage } from './pages/CardPage';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 function App() {
+  let user = false;
   const [cards, setCards] = useState(cardList);
   const [isLoaded, setIsLoaded] = useState(true);
 
-  useEffect(()=> {
-   setTimeout(()=>{
-    setIsLoaded(false);
-   }, 2000)
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(false);
+    }, 2000)
   }, [])
 
 
   function addCard() {
     setCards([
       ...cards,
-      { 
+      {
         id: cards.length + 1,
         theme: "Web Design",
         title: "Название задачи",
         date: "30.10.23",
         status: "Без статуса",
 
-   }
+      }
     ])
 
   }
 
 
   return (
-  <>
-   <GlobalStyle/> 
-   <Routes>
-    <Route path={appRoutes.CARD} element={<CardPage/>}/>
-    <Route path={appRoutes.MAIN} element={<MainPage/>}/>
-    <Route path={appRoutes.EXIT} element={<PopExitPage/>}/>
-    <Route path={appRoutes.LOGIN} element={<LoginPage/>}/>
-    <Route path={appRoutes.REGISTER} element={<RegisterPage/>}/>
-    <Route path={appRoutes.NOT_FOUND} element={<NotFoundPage/>}/>
-   </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route element={<PrivateRoute user={user} />}>
+          <Route path={appRoutes.CARD} element={<CardPage />} />
+          <Route path={appRoutes.MAIN} element={<MainPage />} />
+        </Route>
+        <Route path={appRoutes.EXIT} element={<PopExitPage />} />
+        <Route path={appRoutes.LOGIN} element={<LoginPage />} />
+        <Route path={appRoutes.REGISTER} element={<RegisterPage />} />
+        <Route path={appRoutes.NOT_FOUND} element={<NotFoundPage />} />
+      </Routes>
 
-  </>
+    </>
   );
-  
+
 
 
 }
