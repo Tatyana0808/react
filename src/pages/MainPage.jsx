50a51bac967a141e4aca00b65e9768f454f3aafb
Tatyana0808 <144019586+Tatyana0808@users.noreply.github.com>
2024-01-23 20:19:49 +0300

    сделала изменения

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,7 +8,7 @@ import Main from '../components/Main/Main'
 import { appRoutes } from '../lib/appRoutes';
 import { Link, Outlet } from 'react-router-dom';
 
-export default function MainPage({addCard,isLoaded,cards}) {
+export default function MainPage({ addCard, isLoaded, cards }) {
     return (
         <>
             <Wrapper>
