82df1ce317dd0f387b132aa1d2096864ee91ebfc
Tatyana0808 <144019586+Tatyana0808@users.noreply.github.com>
2024-01-30 18:08:54 +0300

    сделала исправления

--- src/pages/CardPage.jsx
+++ src/pages/CardPage.jsx
@@ -2,5 +2,8 @@ import Card from "../components/Card/Card";
 import PopBrowse from "../components/PopUp/PopBrowse/PopBrowse";
 
 export default function CardPage(){
-    return <PopBrowse/> , <Card/>;
+    return <>
+     <PopBrowse/> 
+     <Card/>
+    </>;
 }
